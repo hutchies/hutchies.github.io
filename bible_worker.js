2fbab435d97c8a809b814cea3aa57f6a3da7fc7d
Mark Hutchinson <Mark@Marks-MBP.broadband>
2020-06-05 11:18:53 +0100

    T

--- bible_worker.js
+++ bible_worker.js
@@ -12,7 +12,7 @@ onmessage = function(event){
         if(response.type == 'basic') return response.text();
         else postMessage({
           'type' : 'log',
-          'data' : response,
+          'data' : response.type,
         })
       })
       .then(function(text){
