6266170f7a902b51c9a284888534b807bd8e042f
Mark Hutchinson <Mark@Marks-MBP.broadband>
2020-06-05 11:06:29 +0100

    Worker

---
+++ bible_worker.js
@@ -0,0 +1,26 @@
+onmessage = function(event){
+  var message = event.data;
+
+  if (message.type === "lookup") {
+    var passage = message.passage;
+    var version = message.version;
+    var url = "https://www.biblegateway.com/passage/?search="+encodeURIComponent(passage)+'&version='+encodeURIComponent(version);
+
+    fetch(url, {method: 'get', mode: 'no-cors'})
+      .then(function(response){
+        if(response.type == 'basic') return response.text();
+        else return 'Error fetching verse';
+      })
+      .then(function(text){
+        postMessage({
+          'type' : 'found',
+          'data' : text,
+          'passage': passage
+        })
+      });
+    }
+}
+
+postMessage({
+  'type' : 'ready'
+});
